a12632cb63e309a1e0d7903935dad2e46a51f8d8
namancg <naman.c@perfios.com>
2021-08-29 23:59:02 +0530

    ADDED: printing arrays using generics

--- src/com/bridgelabz/printarray/PrintArray.java
+++ src/com/bridgelabz/printarray/PrintArray.java
@@ -1,20 +1,27 @@
 package com.bridgelabz.printarray;
-public class PrintArray{
-	public static void main(String[] args) {		
+public class PrintArray<E>{
+	private E[] inputArray;
+	public static void main(String[] args) {
 		Integer[] integerArr = {5,6,7};
-		Double[] doubleArr = {6.33 , 3.89, 7.99};
+		Double[] doubleArr = {6.33,3.89,7.99};
 		Character[] charArr = {'P', 'E', 'R', 'F', 'I', 'O','S'};
-		toPrint(integerArr);
-		toPrint(doubleArr);
-		toPrint(charArr);
-		}
-		
-		
-	private static <e> void toPrint(e[] inputArr) {
-		for(e element : inputArr) {
+		new PrintArray(integerArr).toPrint();
+		PrintArray.toPrint(doubleArr);
+		PrintArray.toPrint(charArr);
+	}
+	public PrintArray(E[] inputArray) {
+		this.inputArray = inputArray;
+	}
+	
+	public void toPrint() {
+		PrintArray.toPrint(this.inputArray);
+	}
+	
+	private static <E> void toPrint(E[] inputArray) {
+		for(E element : inputArray) {
 			System.out.println(element);
 		}
-	}
+	}	
 }
 
 
